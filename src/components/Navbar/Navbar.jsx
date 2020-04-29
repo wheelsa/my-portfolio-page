e02e3d41e7bd96bc01af674404746d595bdd8367
Seth Wheeler <wheeler.seth@gmail.com>
2020-04-29 12:12:40 -0600

    fixed navbar to be responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,8 +6,10 @@ const Navbar = () => {
   useEffect(() => {
     document.addEventListener("scroll", () => {
       let viewPort = window.visualViewport.height
-      if( scroll !== "" && window.scrollY > 0 && window.scrollY < (viewPort/2)){
-        setScroll("")
+
+      if( window.innerWidth > 1200){
+      if( scroll !== "home" && window.scrollY >= 0 && window.scrollY < (viewPort/2)){
+        setScroll("home")
       }else if(scroll !== "about" && window.scrollY >= (viewPort/2) && window.scrollY < (viewPort + 386) ){
         setScroll("about")
       }else if(scroll !== "experience" && window.scrollY > (viewPort + 386) && window.scrollY < (viewPort + 2598)){
@@ -18,7 +20,36 @@ const Navbar = () => {
         setScroll("skills")
       }else if(scroll !== "education" && window.scrollY > (viewPort + 3795) && window.scrollY < (viewPort + 4380)){
         setScroll("education")
+      }} else if( window.innerWidth > 1000 && window.innerWidth <= 1200){
+        if( scroll !== "home" && window.scrollY >= 0 && window.scrollY < (viewPort/2)){
+          setScroll("home")
+        }else if(scroll !== "about" && window.scrollY >= (viewPort/2) && window.scrollY < (viewPort + 470) ){
+          setScroll("about")
+        }else if(scroll !== "experience" && window.scrollY > (viewPort + 470) && window.scrollY < (viewPort + 2360)){
+          setScroll("experience")
+        }else if(scroll!== "projects" && window.scrollY > (viewPort + 2360) && window.scrollY < (viewPort + 2820)){
+          setScroll("projects")
+        }else if(scroll !== "skills" && window.scrollY > (viewPort + 2820) && window.scrollY < (viewPort + 3440)){
+          setScroll("skills")
+        }else if(scroll !== "education" && window.scrollY > (viewPort + 3440)){
+          setScroll("education")
+        }
+      }else if( window.innerWidth > 600 && window.innerWidth <= 1000){
+        if( scroll !== "home" && window.scrollY >= 0 && window.scrollY < (viewPort/2)){
+          setScroll("home")
+        }else if(scroll !== "about" && window.scrollY >= (viewPort/2) && window.scrollY < (viewPort + 470) ){
+          setScroll("about")
+        }else if(scroll !== "experience" && window.scrollY > (viewPort + 470) && window.scrollY < (viewPort + 2360)){
+          setScroll("experience")
+        }else if(scroll!== "projects" && window.scrollY > (viewPort + 2360) && window.scrollY < (viewPort + 2820)){
+          setScroll("projects")
+        }else if(scroll !== "skills" && window.scrollY > (viewPort + 2820) && window.scrollY < (viewPort + 3540)){
+          setScroll("skills")
+        }else if(scroll !== "education" && window.scrollY > (viewPort + 3540)){
+          setScroll("education")
+        }
       }
+
     });
   });
   return (
@@ -34,6 +65,7 @@ const Navbar = () => {
         </div>
       </a>
       <div className="sidebar">
+        <a href="#" className={scroll === "home" ? "active no-display" : "no-display"}>Home</a>
         <a href="#about" className={scroll === "about" ? "active" : null}>About Me</a>
         <a href="#experience" className={scroll === "experience" ? "active" : null}>Experience</a>
         <a href="#projects" className={scroll === "projects" ? "active" : null}>Projects</a>
